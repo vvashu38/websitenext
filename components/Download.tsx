c81ba18746d727a0a4cf3beefa2c438c446d37c0
Talha Tahir <trinitro619@gmail.com>
2024-10-19 20:18:09 +0500

    update css

--- components/Download.tsx
+++ components/Download.tsx
@@ -5,16 +5,16 @@ import { FaApple, FaGooglePlay } from "react-icons/fa";
 const Download: React.FC = () => (
   <section className="container mx-auto py-24 px-4 md:px-6">
     <div className="flex flex-col md:flex-row items-center gap-8">
-      <div className="w-full md:w-1/2 order-2 md:order-1 p-4 justify-start">
+      <div className="w-full md:w-1/2 order-1 p-4 flex justify-center md:justify-start items-center">
         <Image
           src="/products/phone1.png"
           alt="Financial app interface"
           width={500}
           height={500}
-          className="w-1/2 h-auto"
+          className="w-1/2 h-auto mx-auto md:mx-0"
         />
       </div>
-      <div className="w-full md:w-1/2 order-1 md:order-2 flex justify-end">
+      <div className="w-full md:w-1/2 order-2 flex justify-center md:justify-end">
         <div className="flex flex-col justify-center">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-900 dark:text-white mb-4">
             Download Our App
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-white dark:bg-black shadow-sm text-gray-400 py-6">
+    <footer className="bg-white border-t border-gray-100 dark:bg-black shadow-sm text-gray-400 py-6 dark:border-t dark:border-gray-800">
       <div className="container mx-auto px-4 flex flex-wrap justify-center sm:justify-between items-center text-sm">
         <p className="ml-4">&copy; {currentYear} Your Company Name. All rights reserved.</p>
         <div className="flex space-x-4 mt-2 mr-4 sm:mt-0">
